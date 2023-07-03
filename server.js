c6d3f373576abf099b741521f2879bfa3ce9b5d7
Bhumesh <bhumesh1719@gmail.com>
2023-07-03 23:58:50 +0530

    day-1-patterns

---
+++ server.js
@@ -0,0 +1,5 @@
+import { createRectanglePattern, rightagletrianglePattern, rightagletrianglePatternALT, RightAngledNumberPyramid } from './1.rectanglePattern.js';
+// createRectanglePattern(3, 3, '*');
+// rightagletrianglePattern(5);
+// rightagletrianglePatternALT(3);
+RightAngledNumberPyramid(2);
